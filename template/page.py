79b777dc6de0bb0331b3ef565b83d22986e8e925
Richielly <richielly.positivo@gmail.com>
2023-11-23 18:03:58 -0300

    V_1.0.0

--- template/page.py
+++ template/page.py
@@ -1,7 +1,7 @@
 import os
 
 import flet as ft
-from controller import document_loader
+from controller import document_loader, document_spliter, tik_token
 
 def pages(page: ft.Page):
 
@@ -10,7 +10,6 @@ def pages(page: ft.Page):
     page.title = "Gerenciador de arquivos IA" + " V_1.0.0"
     page.icon = "imagem_principal.png"
     progressBar = ft.ProgressBar(width=700, color=ft.colors.DEEP_ORANGE)
-
 
     def pick_files_result(e: ft.FilePickerResultEvent):
 
@@ -41,34 +40,55 @@ def pages(page: ft.Page):
     def btn_click(e):
         resulta_data = []
         text = ""
-        chunk = 0
+        doc_inf = True
         doc = document_loader.DocumentLoader()
-        for pag in doc.load_pdf(selected_files.value):
-            chunk+=1
-            text = text+'\n'+'-'*50 + 'Chunk: '+ str(chunk) +'-'*50 +'\n'+pag.page_content
+        tokens = tik_token.TikToken()
+        total_tokens=0
+        paginas = 0
 
+        for pag in doc.load_pdf(selected_files.value):
+            paginas+=1
+            num_tokens = tokens.num_tokens_from_string(pag.page_content)
+            total_tokens+=num_tokens
+            if doc_inf:
+                text = text+'\n'+'-'*90 + ' → pagina ' +str(pag.metadata['page']+1) +'-'*90 +'\n'+pag.page_content +f'\n 🪙 tokens:  {num_tokens}\n'
+        tokens_total.value = f'💰 {total_tokens} \n 💵 '+ str((total_tokens / 1000) * 0.001)
+        paginas_total.value = f'📃 {paginas}'
         container_1.content=ft.Text(text, selectable=True)
         t.selected_index=1
         page.update()
 
-        # if not txt_database.value:
-        #     txt_database.error_text = "Informe o caminho do Banco"
-        #     page.update()
-        # else:
-        #     page.update()
+    def btn_chunk(e):
+        tokens = tik_token.TikToken()
+        text = ""
+        doc = document_loader.DocumentLoader()
+        document_loaded = doc.load_pdf(selected_files.value)
+        doc_split = document_spliter.DocumentSpliter()
+        result_split = doc_split.split_by_word(document_loaded, chunk_size=int(chunk_size.value))
+        chunks=0
+
+        for split in result_split:
+            num_tokens = tokens.num_tokens_from_string(split.page_content)
+            chunks+=1
+            text = text + '\n' + '-' * 90 + f' → chunk {chunks}' + '-' * 90 + f'\n 🪙 {num_tokens} \n' + split.page_content
+        chunks_total.value = ' 🧩 '+str(chunks) + ' chunks.'
+        container_2.content = ft.Text(text, selectable=True)
+        # print(split.page_content)
+        t.selected_index = 2
+        page.update()
+
 
     page.add(ft.Text(f"Resultado ", size=20, color='blue'))
-    # header_frotas = ft.Text("Gerador de Arquivos das Frotas", size=20, color='blue')
-    # txt_entidade = ft.TextField(label="Entidade", text_size=12, value=cfg['DEFAULT']['CodEntidade'], width=100, height=35, disabled=False, tooltip='Alterar o código de entidade, tambem altera o arquivo "cfg.ini"')
-    # txt_password = ft.TextField(label="Password", text_size=12, value=cfg['DEFAULT']['password'], width=130, height=35, password=True, can_reveal_password=True)
 
-    container_1 = ft.Container(content=ft.Text("Center"), alignment=ft.alignment.center, bgcolor=ft.colors.GREY_200,scale=1)
+    container_1 = ft.Container(content=ft.Text(""), alignment=ft.alignment.center, bgcolor=ft.colors.GREY_200,scale=1)
     btn_gerar_arquivos = ft.ElevatedButton("Gerar Arquivos", on_click=btn_click, icon=ft.icons.ADD_BOX)
-    # list_arquivos = ft.ListView(expand=1, spacing=2, padding=20, auto_scroll=True)
-    # divisor = ft.Divider(height=2, thickness=3)
-
+    btn_gerar_split = ft.ElevatedButton("Iniciar Split", on_click=btn_chunk, icon=ft.icons.ADD_BOX)
+    container_2 = ft.Container(content=ft.Text(""), alignment=ft.alignment.center, bgcolor=ft.colors.GREY_200, scale=1)
+    chunk_size = ft.TextField(label="Tamanho da quebra (chunk)", value=2000)
+    paginas_total = ft.Text('', size=30)
+    tokens_total = ft.Text('Total tokens: 💰', size=30)
+    chunks_total = ft.Text('', size=30)
     header = ft.Text("Pagina", size=20, color='blue')
-
 
     t = ft.Tabs(
         selected_index=0,
@@ -84,13 +104,13 @@ def pages(page: ft.Page):
             ft.Tab(
                 text="Visualizar",
                 icon=ft.icons.VIEW_COZY,
-                content=ft.Container(content=ft.Column([header,container_1],scroll=True), alignment=ft.alignment.center, padding=15),
+                content=ft.Container(content=ft.Column([header,tokens_total,paginas_total,btn_gerar_split, container_1],scroll=True), alignment=ft.alignment.center, padding=15),
             ),
             ft.Tab(
-                text="Configurações",
-                icon=ft.icons.ADMIN_PANEL_SETTINGS,
+                text="Chunks",
+                icon=ft.icons.INSERT_PAGE_BREAK,
                 content=ft.Container(
-                    content=ft.Column([header]), alignment=ft.alignment.center, padding=15
+                    content=ft.Column([header,btn_gerar_split, chunk_size, chunks_total, container_2],scroll=True), alignment=ft.alignment.center, padding=15
                 ),
             ),
         ],
